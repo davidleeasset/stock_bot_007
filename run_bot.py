adf2bf2e6268d9c500a4fee67e1ff7057c0f926e
david.lee <david.lee@ichef.com.tw>
2022-08-10 13:53:50 +0800

    move the file

--- run_bot.py
+++ run_bot.py
@@ -1,7 +1,7 @@
 import discord
 
-from services.dc_bot_actions import DCBotActions
-from settings import DC_BOT_TOKEN, DC_BOT_CONTROL_CHANNEL_IDS, DC_BOT_CONTROL_ROLE_IDS
+from services.dc_bot_actions import process_stock_watching
+from settings import DC_BOT_TOKEN
 
 client = discord.Client()
 
@@ -9,36 +9,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('running as', client.user)
-
-
-async def process_stock_watching(message):
-    if message.channel.type.value == 1:  # is private message
-        pass
-    elif message.channel.id in DC_BOT_CONTROL_CHANNEL_IDS and message.author.top_role.id in DC_BOT_CONTROL_ROLE_IDS:
-        msg = message.content
-        if msg.startswith('--'):
-            bot_actions = DCBotActions(message)
-            msgs = message.content.strip().replace("--", "").split(" ")
-            action = msgs[0]
-
-            if action == "wl":
-                await bot_actions.show_watching_list()
-            elif action == "add_w":
-                if len(msgs) != 2:
-                    await message.channel.send("--add_w takes 2 args.")
-                await bot_actions.add_to_watching_list(msgs[1])
-            elif action == "del_w":
-                if len(msgs) != 2:
-                    await message.channel.send("--del_w takes 2 args.")
-                await bot_actions.delete_from_watching_list(msgs[1])
-            elif action in ["help", "h"]:
-                await message.channel.send(
-                    "--wl :show watching list \n"
-                    "--add_w $symbol : add $symbol to watching list\n"
-                    "--del_w $symbol : remove $symbol from the watching list"
-                )
-            else:
-                await message.channel.send("are you sending a command? use --help")
 
 
 @client.event
--- services/dc_bot_actions.py
+++ services/dc_bot_actions.py
@@ -1,4 +1,5 @@
 from services.mongo_access import MongoDBCollections
+from settings import DC_BOT_CONTROL_CHANNEL_IDS, DC_BOT_CONTROL_ROLE_IDS
 
 
 class DCBotActions:
@@ -19,3 +20,33 @@ class DCBotActions:
         self.mongo.del_watch(symbol)
         watching_list = self.mongo.get_watch_dict().keys()
         await self.message.channel.send(f"已刪除, 更新後的觀察清單: {', '.join(watching_list)}")
+
+
+async def process_stock_watching(message):
+    if message.channel.type.value == 1:  # is private message
+        pass
+    elif message.channel.id in DC_BOT_CONTROL_CHANNEL_IDS and message.author.top_role.id in DC_BOT_CONTROL_ROLE_IDS:
+        msg = message.content
+        if msg.startswith('--'):
+            bot_actions = DCBotActions(message)
+            msgs = message.content.strip().replace("--", "").split(" ")
+            action = msgs[0]
+
+            if action == "wl":
+                await bot_actions.show_watching_list()
+            elif action == "add_w":
+                if len(msgs) != 2:
+                    await message.channel.send("--add_w takes 2 args.")
+                await bot_actions.add_to_watching_list(msgs[1])
+            elif action == "del_w":
+                if len(msgs) != 2:
+                    await message.channel.send("--del_w takes 2 args.")
+                await bot_actions.delete_from_watching_list(msgs[1])
+            elif action in ["help", "h"]:
+                await message.channel.send(
+                    "--wl :show watching list \n"
+                    "--add_w $symbol : add $symbol to watching list\n"
+                    "--del_w $symbol : remove $symbol from the watching list"
+                )
+            else:
+                await message.channel.send("are you sending a command? use --help")
