d580df7da47d7115f7713c9b6115ba2047854715
david.lee <david.lee@ichef.com.tw>
2022-08-11 12:38:30 +0800

    update settings

--- settings.py
+++ settings.py
@@ -1,19 +1,18 @@
-MONGODB_URL = "--"
+import os
 
-DC_BOT_TOKEN = '--'
+import sentry_sdk
+
+MONGODB_URL = os.environ.get("MONGODB_URL", "--")
+
+DC_BOT_TOKEN = os.environ.get("DC_BOT_TOKEN", "--")
 DC_BOT_CONTROL_CHANNEL_IDS = []
 DC_BOT_CONTROL_ROLE_IDS = []
 
-WATCH_HOOK = "--"
-PUBLIC_NEWS_HOOK = "--"
-NEWS_HOOK = "--"
+WATCH_HOOK = os.environ.get("WATCH_HOOK", "--")
+PUBLIC_NEWS_HOOK = os.environ.get("PUBLIC_NEWS_HOOK", "--")
+NEWS_HOOK = os.environ.get("NEWS_HOOK", "--")
 
-import sentry_sdk
 sentry_sdk.init(
-    "--",
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
+    os.environ.get("SENTRY_URL", "--"),
     traces_sample_rate=1.0
 )
